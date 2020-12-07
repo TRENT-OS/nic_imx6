f96ba7bd330c87519fe87d49e4f59ead480751c5
Axel Heider <axel.heider@hensoldt-cyber.de>
2020-12-07 19:00:57 +0100

    [DEV] remove unused include

--- src/ethdriver.c
+++ src/ethdriver.c
@@ -19,7 +19,6 @@
 #include <platsupport/io.h>
 #include <platsupport/irq.h>
 #include <ethdrivers/raw.h>
-#include <ethdrivers/intel.h>
 #include <ethdrivers/helpers.h>
 #include <sel4utils/sel4_zf_logif.h>
 
